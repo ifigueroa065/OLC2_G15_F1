dddea1edc15e5246a11d8d3d705445a8fdc996d3
KathYuman <2993003070101@ingenieria.usac.edu.gt>
2024-06-24 22:17:45 -0600

    instructions

---
+++ FASE2/Frontend/instructions/Msub.js
@@ -0,0 +1,28 @@
+class Msub extends Instruction {
+    constructor(destination, source1, source2, source3, line, col) {
+        super();
+        this.destination = destination;
+        this.source1 = source1;
+        this.source2 = source2;
+        this.source3 = source3;
+        this.line = line;
+        this.col = col;
+    }
+
+    execute(env, gen) {
+        console.log('Executing Msub');
+        console.log('destination', this.destination);
+        console.log('source1', this.source1);
+        console.log('source2', this.source2);
+        console.log('source3', this.source3);
+
+        // Obtener los valores de los operandos
+        let retornoDestino = this.destination?._InsExpr?.execute(env);
+        let retornoSource1 = this.source1?._InsExpr?.execute(env);
+        let retornoSource2 = this.source2?._InsExpr?.execute(env);
+        let retornoSource3 = this.source3?._InsExpr?.execute(env);
+
+        // Realizar la lógica de generación de Quadruples
+        gen.addQuadruple('MSUB', retornoDestino.valor, retornoSource1.valor, retornoSource2.valor, retornoSource3.valor, '-', retornoDestino.valor);
+    }
+}
---
+++ FASE2/Frontend/instructions/Uxtb.js
@@ -0,0 +1,35 @@
+class Uxtb extends Instruction{
+    constructor(destination, source, line, col) {
+        super();
+        // if (!(destination instanceof Expression)) {
+        //     throw new Error('destination must be an instance of Expression');
+        // }
+        this.destination = destination;
+
+        // if (!(source instanceof Expression)) {
+        //     throw new Error('source must be an instance of Expression');
+        // }
+        this.source = source;
+        
+        this.line = line;
+        this.col = col;
+    }
+
+    execute(env, gen) {
+        console.log('Executing UXTB');
+        console.log('destination', this.destination);
+        console.log('source', this.source);
+        // Implementar lógica de instrucción mov básicamente.
+        // 1. Obtener el valor de la variable op1
+            // let _d = destion?.execute(env);
+            let retornoDestino = this.destination?._InsExpr?.execute(env);
+        // 2. Obtener el valor de la variable op2
+            // let _s = source?.execute(env);
+            let retornoSource = this.source?._InsExpr?.execute(env);
+        // 3.1. Realizar la operación de asignación
+            // _d = _s;
+        
+        // 3.2. Realizar la lógica de generación de Quadruples
+        gen.addQuadruple('UXTB', retornoDestino.valor, retornoSource.valor,'-', '-', '-', retornoDestino.valor);
+    }
+}
